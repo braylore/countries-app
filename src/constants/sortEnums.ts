f024271d9fc91992f2611386acb7ae05a8d33a0b
Alexander <braylore27@gmail.com>
2022-05-19 09:29:17 +0300

    sortEnums

---
+++ src/constants/sortEnums.ts
@@ -0,0 +1,13 @@
+export enum SortLabelEnum {
+  Area = 'Area',
+  Name = 'Name',
+  Borders = 'Borders',
+  Population = 'Population'
+}
+
+export enum SortValueEnum {
+  area = 'area',
+  name = 'name',
+  borders = 'borders',
+  population = 'population'
+}
