c2fbacfa4b2f676323fd9f953ce8dc11ffb54a39
Alexander <braylore27@gmail.com>
2022-05-19 05:49:56 +0300

    CountriesListItem

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,16 @@
 import Header from '../Header/Header';
+import CountriesList from '../CountriesList/CountriesList';
+import { useGetAllCountriesQuery } from '../../api/countriesApi';
 
 const App = () => {
-  return <Header />;
+  const { data = [] } = useGetAllCountriesQuery();
+
+  return (
+    <>
+      <Header />
+      <CountriesList countriesList={data} />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/CountriesListItem/CountriesListItem.tsx
@@ -0,0 +1,61 @@
+import { Grid, Typography, Paper, Box } from '@mui/material';
+import { FC } from 'react';
+
+interface ICountriesListItemProps {
+  name: string;
+  flag: string;
+  capital: string;
+  population: string;
+  area: string;
+}
+
+const CountriesListItem: FC<ICountriesListItemProps> = ({ name, flag, capital, population, area }) => {
+  return (
+    <Grid
+      component="li"
+      item
+      xl={4}
+    >
+      <Paper>
+        <img
+          loading="lazy"
+          src={flag}
+          alt={`${name} flag`}
+        />
+        <Box
+          sx={{
+            p: 2
+          }}
+        >
+          <Typography
+            component="h3"
+            sx={{
+              textAlign: 'center',
+              fontSize: '22px',
+              marginBottom: '40px',
+              height: '35px'
+            }}
+          >
+            <b>{name}</b>
+          </Typography>
+          <Typography>
+            <b> Population: </b>
+            {population}
+          </Typography>
+          <Typography>
+            <b>Area: </b>
+            {area}
+            km
+            <sup>2</sup>
+          </Typography>
+          <Typography>
+            <b>Capital: </b>
+            {capital || '-'}
+          </Typography>
+        </Box>
+      </Paper>
+    </Grid>
+  );
+};
+
+export default CountriesListItem;
