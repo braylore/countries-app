23c7be6d5d8c4faf7f72f18471b74621a6ca567a
Alexander <braylore27@gmail.com>
2022-05-19 03:17:03 +0300

    init ColorModeContext

--- src/index.tsx
+++ src/index.tsx
@@ -2,18 +2,23 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, StyledEngineProvider } from '@mui/material';
 import { store } from './store';
 import './index.css';
 import App from './components/App/App';
+import { ColorModeContextProvider } from './context/ColorModeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <ColorModeContextProvider>
+        <CssBaseline />
+        <StyledEngineProvider injectFirst>
+          <App />
+        </StyledEngineProvider>
+      </ColorModeContextProvider>
+    </Provider>
+  </BrowserRouter>
 );
 
