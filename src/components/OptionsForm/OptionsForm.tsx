df97fed53670929cc74c008036aa6e3b55ddfd0e
Alexander <braylore27@gmail.com>
2022-05-19 08:39:15 +0300

    OptionsForm

---
+++ src/components/OptionsForm/OptionsForm.tsx
@@ -0,0 +1,16 @@
+import Accrodion from '../UI/Accordion/Accordion';
+
+const OptionsForm = () => {
+  return (
+    <div>
+      <Accrodion label="Filtration">
+        <div></div>
+      </Accrodion>
+      <Accrodion label="Sorting">
+        <div></div>
+      </Accrodion>
+    </div>
+  );
+};
+
+export default OptionsForm;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,17 @@
 import { FC } from 'react';
 import CountriesList from '../components/CountriesList/CountriesList';
 import { useGetAllCountriesQuery } from '../api/countriesApi';
+import OptionsForm from '../components/OptionsForm/OptionsForm';
 
 const HomePage: FC = () => {
   const { data = [] } = useGetAllCountriesQuery();
 
-  return <CountriesList countriesList={data} />;
+  return (
+    <>
+      <OptionsForm />
+      <CountriesList countriesList={data} />
+    </>
+  );
 };
 
 export default HomePage;
