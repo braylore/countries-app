32e530b0231a16fc639c88f23a6633c42433b79c
Alexander <braylore27@gmail.com>
2022-05-20 09:06:02 +0300

    selectOptions

---
+++ src/constants/selectOptions.ts
@@ -0,0 +1,25 @@
+export const selectOptions = {
+  label: 'Countries per page',
+  elements: [
+    {
+      value: 3,
+      text: '3'
+    },
+    {
+      value: 6,
+      text: '6'
+    },
+    {
+      value: 15,
+      text: '15'
+    },
+    {
+      value: 30,
+      text: '30'
+    },
+    {
+      value: 60,
+      text: '60'
+    }
+  ]
+};
