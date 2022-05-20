ac115375723a64481973430916fec814b8247a16
Alexander <braylore27@gmail.com>
2022-05-20 11:55:59 +0300

    getCountryByName constants  urls

--- src/constants/urls.ts
+++ src/constants/urls.ts
@@ -1,3 +1,5 @@
 export const BASE_URL = 'https://restcountries.com/v3.1/';
 
 export const ALL_COUNTRIES = 'all?fields=name,capital,car,landlocked,flags,region,population,area,unMember,borders';
+
+export const getCountryByName = (name: string) => `name/${name}?fullText=true`;
